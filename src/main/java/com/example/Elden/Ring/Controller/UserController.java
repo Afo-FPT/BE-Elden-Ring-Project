35d9e6f645608a0e94630a16eb0e44e029dd9948
HoangKhuong3007 <hoangkhuong2k4@gmail.com>
2024-10-05 01:20:21 +0700

    add cross port

--- src/main/java/com/example/Elden/Ring/Controller/UserController.java
+++ src/main/java/com/example/Elden/Ring/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.Elden.Ring.Controller;
 import com.example.Elden.Ring.request.UserCreateRequest;
 import com.example.Elden.Ring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,11 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @CrossOrigin  //cross port
+
     @PostMapping("/create-user")
-    String create ( @RequestBody UserCreateRequest request){
-        userService.create(request);
-        return "created";
-    }
+        String create ( @RequestBody UserCreateRequest request){
+            userService.create(request);
+            return "created";
+        }
 }

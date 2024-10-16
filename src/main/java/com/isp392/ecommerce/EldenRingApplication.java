4c9701d7f74a0cbe0e9e75c4da58fc475f595b21
CristianoDinh <dinhgiabao1142004@gmail.com>
2024-10-16 11:04:21 +0700

    test 5

--- src/main/java/com/isp392/ecommerce/EldenRingApplication.java
+++ src/main/java/com/isp392/ecommerce/EldenRingApplication.java
@@ -8,7 +8,7 @@ public class EldenRingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EldenRingApplication.class, args);
-		System.out.println("test ssh 4");
+		System.out.println("test ssh 5");
 	}
 
 }
